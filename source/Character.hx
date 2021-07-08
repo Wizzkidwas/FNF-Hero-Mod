173a5539008074b7832145c4a33642a0800dd88a
Telethia <83116110+Telethia@users.noreply.github.com>
2021-07-08 14:51:47 -0700

    credits, coughing and removing unused shit

--- source/Character.hx
+++ source/Character.hx
@@ -693,6 +693,24 @@ class Character extends FlxSprite
 				animation.addByPrefix('singDOWN', 'Nega Down', 24, false);
 				animation.addByPrefix('singLEFT', 'Nega Left', 24, false);
 				animation.addByPrefix('singRIGHT', 'Nega Right', 24, false);
+				animation.addByPrefix('singDOWN-alt', 'Nega Cough', 24, false);
+					
+				addOffset('idle');
+				addOffset("singUP");
+				addOffset("singRIGHT");
+				addOffset("singLEFT");
+				addOffset("singDOWN");
+				addOffset("singDOWN-alt");
+				
+				playAnim('idle');
+				
+			case 'nega2':
+				frames = Paths.getSparrowAtlas('characters/nega');
+				animation.addByPrefix('idle', 'Nega Idle', 24, false);
+				animation.addByPrefix('singUP', 'Nega Up', 24, false);
+				animation.addByPrefix('singDOWN', 'Nega Down', 24, false);
+				animation.addByPrefix('singLEFT', 'Nega Left', 24, false);
+				animation.addByPrefix('singRIGHT', 'Nega Right', 24, false);
 					
 				addOffset('idle');
 				addOffset("singUP");
